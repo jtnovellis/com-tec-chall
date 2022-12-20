72772e89b9b9efd59ad206c17ed09386d93b1666
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-20 18:58:41 -0500

    feat(home): add pagination and functionalitity to searcher

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,6 +8,18 @@ interface CardProps {
 }
 
 export default function Card({ anime }: CardProps) {
+  const businessRule = (score: number) => {
+    if (score >= 1 && score <= 4) {
+      return 'I do not recommend it';
+    }
+    if (score >= 5 && score <= 7) {
+      return 'You may have fun';
+    }
+    if (score > 7) {
+      return 'Great, this is one of the best anime';
+    }
+  };
+
   return (
     <Link href={`anime/${anime.mal_id}`} className={styles.card}>
       <Image
@@ -19,6 +31,8 @@ export default function Card({ anime }: CardProps) {
       />
       <div className={styles.title}>
         <h3>{anime.title}</h3>
+        <div className={styles.divider} />
+        <p>{businessRule(anime.score)}</p>
       </div>
     </Link>
   );
